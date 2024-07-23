77cebfa564c25e9b8d48657056549005f6e79dac
XBD <1519276452@qq.com>
2024-07-23 11:12:03 +0800

    Query update

--- Query.py
+++ Query.py
@@ -9,26 +9,26 @@ class Query:
     def __init__(self) -> None:
         self.url = 'http://10.64.2.100:30010/business/m5908/entry21'        # ?aac001=&aac002=230304196508294612&aab001=&aae042=&aae041=
         self.headers = {
-            'Access-Token': '7cba52ef-4db0-48c8-8a27-bbc1797e7de5',
+            'Access-Token': 'a0bfc75e-30ed-4ecc-92d2-f26e1a195f52',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
         }
 
-    def query(self):
+    def query(self, ids, year):
         payload = {
             "aac001": "",			        # 个人编号
-            "aac002": "230304196508294612",	# 社会保障号
+            "aac002": ids,	# 社会保障号
             "aab001": "",   		        # 单位编号
-            "aae042": "202212",		        # 终止年月
-            "aae041": "202201",		        # 开始年月	
+            "aae042": f"{year}12",		        # 终止年月
+            "aae041": f"{year}01",		        # 开始年月	
             "page": "1",
-            "rows": "12"
+            "rows": "30"
         }
         res = requests.post(url=self.url, data=payload, headers=self.headers)
         if res.status_code == 200:
-            print(res.json())
+            # print(res.json())
+            error = res.json().get('error')
             rows = res.json().get('rows')
-            if rows:
-                print(rows)
+            if isinstance(rows, list) and not error:
                 print(f'请求结果:{res.json().get("total")}条')
                 for row in rows:
                     dic = {
@@ -46,11 +46,22 @@ class Query:
                         '数据来源': row.get('aae741')
                     }
                     ic(dic.values())
+            else:
+                print(f'{ids}-{error}')
         else:
             print(res.status_code)
+    
+    def read(self):
+        from openpyxl import load_workbook
+        wb = load_workbook(r'C:/Users/XBD/Desktop/大半道查询.xlsx')
+        ws = wb['Sheet1']
+        id_card = [i.value for i in ws['E']][2:82]
+        for ids in id_card:
+            self.query(ids, '2022')
     
 
 if __name__ == '__main__':
     Q = Query()
-    Q.query()
-    
+    # Q.read()
+    Q.query('230304197202034441', '2022')
+
